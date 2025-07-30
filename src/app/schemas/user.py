c2ecc98ca2cdf76156764c7f4697c6515a3fea25
Cyt1xxx <kakietopa933@gmail.com>
2025-07-31 02:58:22 +0300

    feat: created pydantic model for user data

---
+++ src/app/schemas/user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+class UserCreate(BaseModel):
+    username: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=8)
+    email: EmailStr | None = None
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(None, min_length=3, max_length=50)
+    password: str | None = Field(None, min_length=8)
+    email: EmailStr | None = None
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr | None = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class Token(BaseModel):
+    acess_token: str
+    token_type: str
