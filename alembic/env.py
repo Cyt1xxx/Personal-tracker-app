12732d1605c4a4ec42fdb2afcd606f54bdc00433
Cyt1xxx <kakietopa933@gmail.com>
2025-07-31 01:29:09 +0300

    feat: setup alembic and add users table migration

--- alembic/env.py
+++ alembic/env.py
@@ -14,11 +14,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from src.app.database.base import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -57,11 +54,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    from sqlalchemy import engine_from_config, pool
+    from src.app.database.database import engine
+
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
