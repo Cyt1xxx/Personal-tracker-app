363503eaa9c883fe3e2b14bc7bb781d51100908f
Cyt1xxx <kakietopa933@gmail.com>
2025-08-03 00:50:20 +0300

    feat: funtction to encrypt a password by algorithm

--- src/app/crud/user.py
+++ src/app/crud/user.py
@@ -1,9 +1,15 @@
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 from src.app.models.user import User
 from src.app.schemas.user import UserCreate
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
 def get_password_hash(password: str) -> str:
-    return password + "_hashed"
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_user(db: Session, user: UserCreate) -> User:
     hashed_password = get_password_hash(user.password)
