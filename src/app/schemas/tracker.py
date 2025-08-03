61b7176b293f29eb223c022a878aba5cdfb18de4
Cyt1xxx <kakietopa933@gmail.com>
2025-08-04 02:20:31 +0300

    feat: did Pydantic scheme for tracker.py. 2 schemes for request and response

---
+++ src/app/schemas/tracker.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
+
+class TrackerEntryCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class TrackerEntryResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    created_at: datetime
+    owner_id: int
+
+    model_config = ConfigDict(from_attributes=True)
