daf84ca46915e7b9daf073723b8416ba6a4fc8a5
Cyt1xxx <kakietopa933@gmail.com>
2025-08-03 03:36:39 +0300

    feat: did get_current_user_from_token function. it checks tokens validation. if it is hacked, expired by jwt.decode func, returns TokenData obj if everything is good for FastAPI dependencies in endpoints for /me

--- src/app/security.py
+++ src/app/security.py
@@ -1,9 +1,14 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt
 from src.app.core.config import get_settings
+from src.app.schemas.user import TokenData
 
 settings = get_settings()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/token")
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -14,3 +19,23 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+
+def get_current_user_from_token(token: str = Depends(oauth2_scheme)) -> TokenData:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username = payload.get("sub")
+
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+
