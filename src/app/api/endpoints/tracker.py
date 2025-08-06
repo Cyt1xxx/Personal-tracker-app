5d04b2e0ba59b0b0d2841990740c18e763227354
Cyt1xxx <kakietopa933@gmail.com>
2025-08-06 23:46:30 +0300

    feat: created new api endpoint for tracker routes. made endpoint create_tracker_entry post request

---
+++ src/app/api/endpoints/tracker.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from src.app.models.user import User
+from src.app.schemas.tracker import TrackerEntryCreate, TrackerEntryResponse
+from src.app.crud.tracker import create_tracker_entry
+from src.app.database.database import get_db
+from src.app.security import get_current_user_from_token
+
+router = APIRouter(
+    prefix="/tracker",
+    tags=["tracker"]
+)
+
+@router.post("/", response_model=TrackerEntryResponse, status_code=status.HTTP_201_CREATED)
+def create_tracker_entry(
+    entry: TrackerEntryCreate,
+    current_user: User = Depends(get_current_user_from_token),
+    db: Session = Depends(get_db)
+):
+    
+    return create_tracker_entry(db=db, entry=entry, user_id=current_user.id)
