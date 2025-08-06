33df342fdf2c328d985ae7830441df9e6c2c9e73
Cyt1xxx <kakietopa933@gmail.com>
2025-08-06 23:44:35 +0300

    refactor: now uses user model to return users id

--- src/app/security.py
+++ src/app/security.py
@@ -3,8 +3,13 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 from typing import Optional
 from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from src.app.models.user import User
+from src.app.database.database import get_db
 from src.app.core.config import get_settings
 from src.app.schemas.user import TokenData
+from src.app.crud.user import get_user_by_username
 
 settings = get_settings()
 
@@ -20,7 +25,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
-def get_current_user_from_token(token: str = Depends(oauth2_scheme)) -> TokenData:
+def get_current_user_from_token(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -37,5 +42,9 @@ def get_current_user_from_token(token: str = Depends(oauth2_scheme)) -> TokenDat
     except JWTError:
         raise credentials_exception
     
-    return token_data
+    db_user = get_user_by_username(db, username=token_data.username)
+    if db_user is None:
+        raise credentials_exception
+    
+    return db_user
 
