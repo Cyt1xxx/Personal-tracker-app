63601608ade83382b97388733d1aa22a2b250538
Cyt1xxx <kakietopa933@gmail.com>
2025-08-07 19:46:13 +0300

    feat: created new crud function to search entries with its title

--- src/app/crud/tracker.py
+++ src/app/crud/tracker.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
+from typing import List
+
 from src.app.models.tracker import TrackerEntry
 from src.app.schemas.tracker import TrackerEntryCreate
-from typing import List
+
 
 def create_tracker_entry(db: Session, entry: TrackerEntryCreate, user_id: int) -> TrackerEntry:
     db_entry = TrackerEntry(
@@ -20,3 +22,9 @@ def create_tracker_entry(db: Session, entry: TrackerEntryCreate, user_id: int) -
 
 def get_tracker_entries_by_user(db: Session, user_id: int) -> List[TrackerEntry]:
     return db.query(TrackerEntry).filter(TrackerEntry.owner_id == user_id).all()
+
+def search_tracker_entries_by_title(db: Session, title: str, user_id: int) -> List[TrackerEntry]:
+    return db.query(TrackerEntry).filter(
+        TrackerEntry.owner_id == user_id,
+        TrackerEntry.title.ilike(f"%{title}%")
+    ).all()
