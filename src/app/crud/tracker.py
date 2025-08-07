afefeb3134aa4c027871994107f5d2843de38f4a
Cyt1xxx <kakietopa933@gmail.com>
2025-08-07 18:39:34 +0300

    feat: created crud function of getting trackerentry obj from table tracker_entries

--- src/app/crud/tracker.py
+++ src/app/crud/tracker.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from src.app.models.tracker import TrackerEntry
 from src.app.schemas.tracker import TrackerEntryCreate
+from typing import List
 
 def create_tracker_entry(db: Session, entry: TrackerEntryCreate, user_id: int) -> TrackerEntry:
     db_entry = TrackerEntry(
@@ -17,3 +18,5 @@ def create_tracker_entry(db: Session, entry: TrackerEntryCreate, user_id: int) -
 
     return db_entry
 
+def get_tracker_entries_by_user(db: Session, user_id: int) -> List[TrackerEntry]:
+    return db.query(TrackerEntry).filter(TrackerEntry.owner_id == user_id).all()
