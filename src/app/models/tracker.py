566887b03566d2cb33c2e57838263c3568141657
Cyt1xxx <kakietopa933@gmail.com>
2025-08-08 17:24:29 +0300

    feat: created uniqueconstraint for title in column owner_id

--- src/app/models/tracker.py
+++ src/app/models/tracker.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
@@ -7,9 +7,13 @@ from src.app.database.database import Base
 class TrackerEntry(Base):
     __tablename__ = "tracker_entries"
 
+    __table_args__ = (
+        UniqueConstraint("owner_id", "title", name="_owner_id_title_uc"),
+    )
+
     id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String(100), index=True)
+    description = Column(String(500))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     owner_id = Column(Integer, ForeignKey("users.id"))
 
