b340574f7a5de6eb8aeaaae25ccbe6aabda25239
Cyt1xxx <kakietopa933@gmail.com>
2025-08-08 22:34:16 +0300

    feat: did PUT router for Update crud function , returns updated fiels of entry

--- src/app/api/endpoints/tracker.py
+++ src/app/api/endpoints/tracker.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 
 from typing import List
 from src.app.models.user import User
-from src.app.schemas.tracker import TrackerEntryCreate, TrackerEntryResponse
+from src.app.models.tracker import TrackerEntry
+from src.app.schemas.tracker import TrackerEntryCreate, TrackerEntryResponse, TrackerEntryUpdate
 from src.app.crud import tracker as crud_tracker
 from src.app.database.database import get_db
 from src.app.security import get_current_user_from_token
@@ -38,3 +39,18 @@ def sarch_tracker_entries(
 ):
     entries = crud_tracker.search_tracker_entries_by_title(db=db, title=title, user_id=current_user.id)
     return entries 
+
+@router.put("/{entry_id}", response_model=TrackerEntryUpdate, status_code=status.HTTP_200_OK)
+def update_tracker_entry_api(
+    entry: TrackerEntryUpdate,
+    entry_id: int,
+    current_user: User = Depends(get_current_user_from_token),
+    db: Session = Depends(get_db),
+):
+    entry_update = crud_tracker.update_tracker_entry(db=db, entry_id=entry_id, user_id=current_user.id, entry_update=entry)
+    if not entry_update:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Entry not found"
+        )
+    return entry_update
