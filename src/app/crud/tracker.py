32a0cb9bb4b59cc40adb52a7f0b79d3821a96c27
Cyt1xxx <kakietopa933@gmail.com>
2025-08-08 22:32:59 +0300

    feat: did CRUD function to update track_entry , set exclude_unset = True , to be able to change only 1 arg , returns TrackerEntry object or None

--- src/app/crud/tracker.py
+++ src/app/crud/tracker.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from src.app.models.tracker import TrackerEntry
-from src.app.schemas.tracker import TrackerEntryCreate
+from src.app.schemas.tracker import TrackerEntryCreate, TrackerEntryUpdate
 
 
 def create_tracker_entry(db: Session, entry: TrackerEntryCreate, user_id: int) -> TrackerEntry:
@@ -28,3 +28,20 @@ def search_tracker_entries_by_title(db: Session, title: str, user_id: int) -> Li
         TrackerEntry.owner_id == user_id,
         TrackerEntry.title.ilike(f"%{title}%")
     ).all()
+
+def update_tracker_entry(db: Session, entry_id: int, user_id: int, entry_update: TrackerEntryUpdate) -> TrackerEntryUpdate | None:
+    db_entry = db.query(TrackerEntry).filter(
+        TrackerEntry.id == entry_id,
+        TrackerEntry.owner_id == user_id     
+    ).first()
+
+    if not db_entry:
+        return None
+
+    update_data = entry_update.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_entry, key, value)
+
+    db.commit()
+
+    return db_entry
