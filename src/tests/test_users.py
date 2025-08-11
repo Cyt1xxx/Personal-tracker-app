c2e5098cc116ae9297f0f1d47cf2679ed58f0707
Cyt1xxx <kakietopa933@gmail.com>
2025-08-11 18:30:16 +0300

    feat: created test_create_user test

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,13 +1,14 @@
-from fastapi.testclient import TestClient
-from src.app.main import app
-
-client = TestClient(app)
-
-def test_create_user():
+def test_create_user(client):
     user_data = {
-        "username": "BobTheGoa809",
+        "username": "BobTheGoa8095",
         "password": "12345678",
         "email": None
     }
     response = client.post("/users/", json=user_data)
     assert response.status_code == 201
+    assert "password" not in response.json()
+    assert "username" in response.json()
+    assert "id" in response.json()
+    assert "email" in response.json()
+    assert "created_at" in response.json()
+
