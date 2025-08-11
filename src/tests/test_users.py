ddd31ee818b9e16e8c1457378317dac010a53119
Cyt1xxx <kakietopa933@gmail.com>
2025-08-11 20:51:51 +0300

    feat: did test for authentification of user and loginning

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -55,3 +55,47 @@ def test_login_for_access_token(client):
     assert "access_token" in response.json()
 
 
+def test_login_with_invalid_password(client):
+    user_data = {
+        "username": "BobTheGoa8095",
+        "password": "12345678",
+        "email": None
+    }
+    client.post("/users/", json=user_data)
+    response = client.post(
+        "/users/token",
+        data={
+            "username": "BobTheGoa8095",
+            "password": "fake pass",
+        }
+    )
+    assert response.status_code == 400
+    
+def test_read_current_user(client):
+    user_data = {
+        "username": "BobTheGoa8095",
+        "password": "12345678",
+        "email": None
+    }
+    client.post("/users/", json=user_data)
+
+    token_r = client.post(
+        "/users/token",
+        data={
+            "username": "BobTheGoa8095",
+            "password": "12345678",
+        }
+    )
+    token = token_r.json().get("access_token")
+    response = client.get("/users/me/", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+    assert "password" not in response.json()
+    assert "username" in response.json()
+    assert "id" in response.json()
+    assert "email" in response.json()
+    assert "created_at" in response.json()
+
+def test_read_current_user_without_auth(client):
+    response = client.get("/users/me/")
+    assert response.status_code == 401
+
