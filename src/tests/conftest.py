28661cd2c33082e9a8f1e4ed2cbd1ec48532783f
Cyt1xxx <kakietopa933@gmail.com>
2025-08-11 17:46:16 +0300

    feat: created conftest single-use bd for tests

---
+++ src/tests/conftest.py
@@ -0,0 +1,43 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from src.app.database.base import Base
+from src.app.database.database import get_db
+from src.app.main import app
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def db():
+    Base.metadata.create_all(bind=engine)
+    db = TestSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture()
+def client(db):
+    def override_get_db():
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    
+    with TestClient(app) as c:
+        yield c
+    
+    app.dependency_overrides.clear()
+
+
+    
