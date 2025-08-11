8bf2c36fd7b93875bd70c063c44a71f3381c7197
Cyt1xxx <kakietopa933@gmail.com>
2025-08-11 21:04:02 +0300

    fix and feat: made using test_user_data from fixture in test functions, created test_functions with creating existing user and invalid email, made some comments

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -2,12 +2,8 @@ def test_get_api_endpoiny(client):
     response = client.get("/")
     assert response.status_code == 200
 
-def test_create_user(client):
-    user_data = {
-        "username": "BobTheGoa8095",
-        "password": "12345678",
-        "email": None
-    }
+def test_create_user(client, test_user_data):
+    user_data = test_user_data
     response = client.post("/users/", json=user_data)
     assert response.status_code == 201
     assert "password" not in response.json()
@@ -16,33 +12,47 @@ def test_create_user(client):
     assert "email" in response.json()
     assert "created_at" in response.json()
 
+def test_create_existing_user(client, test_user_data):
+    user_data = test_user_data
+    client.post("/users/", json=user_data)
+
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 400
+
+def test_create_user_with_invalid_email(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "BobTheGoa8095",
+            "email": "user2121 123 !!!", #Checkup validation for pydantic model EmailStr
+            "password": "12345678" 
+        }
+    )
+
+    assert response.status_code == 422
+
 def test_create_user_with_invalid_password(client):
     response = client.post(
         "/users/",
         json={
             "username": "BobTheGoa8095",
             "email": "user2121123@example.com",
-            "password": "1"
+            "password": "1" #Checkup for pydantic model min_length = 8
         }
     )
     assert response.status_code == 422
 
-def test_get_user(client):
-    user_data = {
-        "username": "BobTheGoa8095",
-        "password": "12345678",
-        "email": None
-    }
+def test_get_user(client, test_user_data):
+    #making new user for test
+    user_data = test_user_data
     response = client.post("/users/", json=user_data)
+
+    #getting info of created user
     response = client.get(f"/users/{user_data["username"]}")
     assert response.status_code == 200
 
-def test_login_for_access_token(client):
-    user_data = {
-        "username": "BobTheGoa8095",
-        "password": "12345678",
-        "email": None
-    }
+def test_login_for_access_token(client, test_user_data):
+    user_data = test_user_data
     client.post("/users/", json=user_data)
     response = client.post(
         "/users/token",
@@ -55,12 +65,8 @@ def test_login_for_access_token(client):
     assert "access_token" in response.json()
 
 
-def test_login_with_invalid_password(client):
-    user_data = {
-        "username": "BobTheGoa8095",
-        "password": "12345678",
-        "email": None
-    }
+def test_login_with_invalid_password(client, test_user_data):
+    user_data = test_user_data
     client.post("/users/", json=user_data)
     response = client.post(
         "/users/token",
@@ -71,12 +77,8 @@ def test_login_with_invalid_password(client):
     )
     assert response.status_code == 400
     
-def test_read_current_user(client):
-    user_data = {
-        "username": "BobTheGoa8095",
-        "password": "12345678",
-        "email": None
-    }
+def test_read_current_user(client, test_user_data):
+    user_data = test_user_data
     client.post("/users/", json=user_data)
 
     token_r = client.post(
